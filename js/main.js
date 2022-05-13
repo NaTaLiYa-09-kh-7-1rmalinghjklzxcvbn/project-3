74e9d9107744aac1001af41274fd9b0e62f37e34
gitbrains <info@geekbrains.ru>
2022-05-13 23:12:54 +0300

    lesson 2

--- js/main.js
+++ js/main.js
@@ -1,36 +1,98 @@
-const products = [
-  { id: 1, title: 'Notebook', price: 1000 },
-  { id: 2, title: 'Mouse', price: 100 },
-  { id: 3, title: 'Keyboard', price: 250 },
-  { id: 4, title: 'Gamepad', price: 150 },
-];
+class ProductList {
+  constructor(container = '.products') {
+    this.container = document.querySelector(container);
+    this.goods = [];
+    this.productObjects = [];
 
-const getProductHTMLString = (title, price, images) =>
-  `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price} p.</p>
-                <img class = "product-img" src = ${images =
-  'https://st2.depositphotos.com/4641393/9871/i/600/' +
-  'depositphotos_98718182-stock-photo-soft-colored-abstract-background-for.jpg\
-                   width = 50%'} alt = >
-                 <button class="by-btn">Добавить</button>
-              </div>`;
+    this.fetchGoods();
+    this.render();
+    this.getSum();
+  }
 
+  fetchGoods() {
+    this.goods = [
+      { id: 1, title: 'Notebook', price: 20000 },
+      { id: 2, title: 'Mouse', price: 1500 },
+      { id: 3, title: 'Keyboard', price: 5000 },
+      { id: 4, title: 'Gamepad', price: 4500 },
+    ];
+  }
 
-const renderProducts = productList => {
-  const list = productList.map((good) => getProductHTMLString(good.title, good.price));
+  render() {
+    for (const good of this.goods) {
+      const productObject = new ProductItem(good);
+      console.log(productObject);
+      this.productObjects.push(productObject);
 
-  document.querySelector('.products').innerHTML = list.join('');
-  // console.log(list);
+      this.container.insertAdjacentHTML('beforeend', productObject.getHTMLString());
+    }
+  }
+  getSum() {
+    const sum = this.goods.reduce((acc, product) => acc + product.price, 0);
+    console.log(sum);
+  }
+}
 
-};
-//Почему через метод 'reduce' первый продукт выводится objekt Objekt
-/*const renderProducts = (productList) => {
-  const list = productList.reduce((acc, good) => acc + getProductHTMLString(good.title, good.price));
-  document.querySelector('.products').innerHTML = list;
-  console.log(list);
-}*/
+class ProductItem {
+  constructor(product, img = 'https://via.placeholder.com/200x150') {
+    this.id = product.id;
+    this.title = product.title;
+    this.price = product.price;
+    this.img = img;
+  }
 
+  getHTMLString() {
+    return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} \u20bd</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`;
+  }
+}
 
-renderProducts(products);
+class Basket {
+  constructor() {
+    this.goods = [];
 
+    this.addToBasket();
+    this.deleteFromBasket();
+  }
+}
+
+class Cart {
+  constructor(product) {
+    this.id = product.id;
+    this.title = product.title;
+    this.price = product.price;
+  }
+}
+
+new ProductList();
+
+// const products = [
+//   {id: 1, title: 'Notebook', price: 20000},
+//   {id: 2, title: 'Mouse', price: 1500},
+//   {id: 3, title: 'Keyboard', price: 5000},
+//   {id: 4, title: 'Gamepad', price: 4500},
+// ];
+//
+// const renderProduct = (item, img='https://via.placeholder.com/200x150') => `<div class="product-item">
+//               <img src="${img}" alt="Some img">
+//               <div class="desc">
+//                   <h3>${item.title}</h3>
+//                   <p>${item.price} \u20bd</p>
+//                   <button class="buy-btn">Купить</button>
+//               </div>
+//           </div>`;
+//
+// const renderProducts = list => {
+//   document
+//       .querySelector('.products')
+//       .insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(''));
+// };
+//
+// renderProducts(products);
+//

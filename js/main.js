d458487f7f8ece735ec35c4a0f59583857ec39a6
gitbrains <info@geekbrains.ru>
2022-06-02 20:57:06 +0300

    lesson 6

--- js/main.js
+++ js/main.js
@@ -3,95 +3,19 @@ const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-a
 const app = new Vue({
     el: '#app',
     data: {
-        catalogUrl: '/catalogData.json',
-        products: [],
-        imgCatalog: 'https://via.placeholder.com/200x150',
-        allProducts: [],
-        basketUrl: '/getBasket.json',
         userSearch: '',
-        showCart: false,
-        imgCart: 'https://via.placeholder.com/50x100',
-        filtered: []
     },
-
     methods: {
-
         getJson(url) {
             return fetch(url)
                 .then(result => result.json())
                 .catch(error => {
-                    console.log(error);
-                })
+                    this.$refs.error.setError(error);//.isHidden();
+                });
         },
-        addProduct(product) {
-            this.getJson(`${API}/addToBasket.json`)
-                .then(data => {
-                    if (data.result === 1) {
-                        let find = this.allProducts.find(el => el.id_product === product.id_product);
-                        if (find) {
-                            find.quantity++;
-                        } else {
-                            let prod = Object.assign({ quantity: 1 }, product);
-                            this.allProducts.push(prod)
-                        }
-                    } else {
-                        alert('Error');
-                    }
-                })
-        },
-        remove(item) {
-            this.getJson(`${API}/deleteFromBasket.json`)
-                .then(data => {
-                    if (data.result === 1) {
-                        if (item.quantity > 1) {
-                            item.quantity--;
-                        } else {
-                            this.allProducts.splice(this.allProducts.indexOf(item), 1)
-                        }
-                    }
-                })
-        },
-        filter() {
-            let regexp = new RegExp(this.userSearch, 'i');
-            this.filtered = this.products.filter(el => regexp.test(el.product_name));
-        },
-    },
-
-    beforeCreate() {
-        console.log('beforeCreate');
-    },
-    created() {
-
-
-    },
-    beforeMount() {
-        console.log('beforeMount');
     },
     mounted() {
-        this.getJson(`${API}` + this.basketUrl)
-            .then(data => {
-                for (let el of data.contents) {
-                    this.allProducts.push(el);
-                }
-            });
-        this.getJson(`${API + this.catalogUrl}`)
-            .then(data => {
-                for (let el of data) {
-                    this.products.push(el);
-                }
-            });
-    },
-    beforeUpdate() {
-        console.log('beforeUpdate');
-    },
-    updated() {
-        console.log('updated');
-    },
-    beforeDestroy() {
-        console.log('beforeDestroy');
-    },
-    destroyed() {
-        console.log('destroyed');
-    },
+        console.log(this);
+    }
 });
 
